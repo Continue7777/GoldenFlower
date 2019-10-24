c9ecf4a0bed6402718d7d23f58df5a3d9823c49c
fanyu.zhang <fanyu.zhang@beibei.com>
2019-10-24 13:36:17 +0800

    fix bug

--- DQN.py
+++ DQN.py
@@ -90,8 +90,8 @@ class DQN:
 
         card_layer = tf.layers.dense(self.playCardsEmb, self.card_layer_unit,activation=tf.nn.leaky_relu)
 
-        self.predictionsNotSee = tf.layers.dense(tf.nn.relu(tf.layers.dense(self.output_fw[:,-1,:], 10)),len(self.action_notsee_index_dicts)) # bs,notsee + 1
-        self.predictionsSee = tf.layers.dense(tf.nn.relu(tf.layers.dense(tf.concat([self.output_fw[:, -1, :], card_layer], 1), 10)),len(self.action_see_index_dicts)) # bs,see
+        self.predictionsNotSee = tf.layers.dense(tf.nn.relu(tf.layers.dense(self.last_output, 10)),len(self.action_notsee_index_dicts)) # bs,notsee + 1
+        self.predictionsSee = tf.layers.dense(tf.nn.relu(tf.layers.dense(tf.concat([self.last_output, card_layer], 1), 10)),len(self.action_see_index_dicts)) # bs,see
         self.prediction = tf.concat([self.predictionsNotSee[:,:-1],self.predictionsSee],1) # bs see+not_see
         self.maskOutput = tf.gather(self.mask,self.personStatusInput * tf.cast(~tf.equal(tf.arg_max(self.predictionsNotSee,1),len(self.action_notsee_index_dicts)),dtype=tf.int32))
         # 看 看 看 0
